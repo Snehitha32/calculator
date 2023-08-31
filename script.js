6c7b963829fe029beb8f7df63768b5a7efd252f6
Snehitha32 <143730631+Snehitha32@users.noreply.github.com>
2023-08-31 20:57:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let currentExpression = '';
+
+function appendNumber(number) {
+  currentExpression += number;
+  updateDisplay();
+}
+
+function appendOperator(operator) {
+  currentExpression += operator;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentExpression = '';
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    const result = eval(currentExpression);
+    currentExpression = result.toString();
+    updateDisplay();
+  } catch (error) {
+    currentExpression = 'Error';
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = currentExpression;
+}
